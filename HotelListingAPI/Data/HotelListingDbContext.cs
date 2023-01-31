0d487e6cb0cfed7b24fa49c030b652c41881c2f4
PanJakub97 <kuba.stasiak78@gmail.com>
2023-01-31 18:28:56 +0100

    Added Enitity

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Poland",
+                    ShortName = "POL",
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Jamaica",
+                    ShortName = "JM",
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Bahamas",
+                    ShortName = "BS",
+                },
+                new Country
+                {
+                    Id = 4,
+                    Name = "Dominicana",
+                    ShortName = "DOM",
+                }
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Gołębiewski",
+                    Address = "Karpacz",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 2,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Comfort Resort and Spa",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.7
+                },
+                new Hotel
+                {
+                    Id = 4,
+                    Name = "Kambii Resort and Spa",
+                    Address = "Bambito",
+                    CountryId = 4,
+                    Rating = 3
+                }
+                );
+        }
+    }
+}
---
+++ HotelListingAPI/Migrations/20230131171949_SeededCountriesAndHotels.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededCountriesAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Poland", "POL" },
+                    { 2, "Jamaica", "JM" },
+                    { 3, "Bahamas", "BS" },
+                    { 4, "Dominicana", "DOM" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Karpacz", 1, "Gołębiewski", 4.5 },
+                    { 2, "Negril", 2, "Sandals Resort and Spa", 4.5 },
+                    { 3, "George Town", 3, "Comfort Resort and Spa", 4.7000000000000002 },
+                    { 4, "Bambito", 4, "Kambii Resort and Spa", 3.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
