21f74b4ea2b0d06a434931e05bbce12db5a26983
PanJakub97 <kuba.stasiak78@gmail.com>
2023-03-23 21:50:04 +0100

    Fix Scaffolding and Migrations

--- HotelListingAPI.Data/HotelListingDbContext.cs
+++ HotelListingAPI.Data/HotelListingDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using HotelListingAPI.Data.Configuration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace HotelListingAPI.Data
 {
@@ -20,6 +22,22 @@ namespace HotelListingAPI.Data
             modelBuilder.ApplyConfiguration(new RoleConfiguration());
             modelBuilder.ApplyConfiguration(new CountryConfiguration());
             modelBuilder.ApplyConfiguration(new HotelConfiguration());
+        }
+    }
+
+    public class HotelListingDbContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
+    {
+        public HotelListingDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
+            var conn = config.GetConnectionString("HotelListingDbConnectionString");
+            optionsBuilder.UseSqlServer(conn);
+            return new HotelListingDbContext(optionsBuilder.Options);
         }
     }
 }
